465c5040776b497d5114f36986b0de70f0fbc8bb
hdahhaoui <d_hachimi@yahoo.com>
2025-11-16 00:20:37 +0100

    Update pocketbase.ts

--- frontend/src/config/pocketbase.ts
+++ frontend/src/config/pocketbase.ts
@@ -4,7 +4,8 @@ const pb = new PocketBase(
   import.meta.env.VITE_POCKETBASE_URL || 'http://127.0.0.1:8090'
 );
 
-// Force /api à la fin
-pb.baseUrl = pb.baseUrl.replace(/\/$/, '') + '/api';
+pb.autoCancellation(false);
 
+// Surtout : NE PAS ajouter /api ici
 export default pb;
+
