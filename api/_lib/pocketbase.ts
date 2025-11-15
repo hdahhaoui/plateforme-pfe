b6b1bde1b8ff988e3729fbf9ab6fc6a16d4e7942
hdahhaoui <d_hachimi@yahoo.com>
2025-11-15 18:05:02 +0100

    Update pocketbase.ts

--- api/_lib/pocketbase.ts
+++ api/_lib/pocketbase.ts
@@ -1,19 +1,10 @@
 import PocketBase from 'pocketbase';
 
-function assertEnv(key: string) {
-  if (!process.env[key]) {
-    throw new Error(`Variable d'environnement ${key} manquante`);
-  }
-  return process.env[key]!;
-}
+const pb = new PocketBase(
+  import.meta.env.VITE_POCKETBASE_URL
+    ?? 'https://pocketbase-fly-late-glade-7750.fly.dev'
+);
 
-export async function getPocketBaseAdmin() {
-  const url = assertEnv('POCKETBASE_URL');
-  const email = assertEnv('POCKETBASE_ADMIN_EMAIL');
-  const password = assertEnv('POCKETBASE_ADMIN_PASSWORD');
+pb.autoCancellation(false);
 
-  const pb = new PocketBase(url);
-  pb.autoCancellation(false);
-  await pb.admins.authWithPassword(email, password);
-  return pb;
-}
+export default pb;
